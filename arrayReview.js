52c882c1951daf3df17be1f40117bbe3510e3092
Michael Telford <mgtelford@gmail.com>
2015-01-14 21:22:59 -0700

    Array Review Completed

--- arrayReview.js
+++ arrayReview.js
@@ -2,7 +2,9 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+var last = function() {
+	alert(threeItems[threeItems.length - 1]);
+};
 
 
 
@@ -13,31 +15,50 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+for (var i = 0; i < evenArray.length; i++) {
+	if (evenArray[i] % 2 !== 0) {
+		evenArray.splice(i, 1);
+		i--;
+	}
+}
+console.log(evenArray);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 
+//'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if 
+//that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+var randomNumber = function() {
+  var x = getRandomArbitrary();
+  console.log(x);
+  for (var i = 0; i < randomArray.length; i++) {
+    if (randomArray[i] === x) {
+      return true;
+    } 
+  }
+      return false;
+}
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, 
+//first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+var second = first.slice(0);
+second.push(6, 7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -51,7 +72,19 @@ alert(second) //[1,2,3,4,5,6,7];
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
 var sentence = "Dev Mountain is the best"
 
-  //code here
+var longest = function () {
+  var sen = sentence.split(" ");
+  console.log(sen);
+  var longest = 0;
+  var word;
+  for (var i = 0; i < sen.length - 1; i++) {
+    if (longest < sen[i].length) {
+      longest = sen[i].length;
+      word = sen[i];
+    }
+  }
+  return word;
+}
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -59,10 +92,16 @@ var sentence = "Dev Mountain is the best"
 
 
 //write a function called capitalize that takes in the myPoem variable and capitalizes every word 
-var myPoem = 'What is a jQuery but a misunderstood object?'
+var myPoem = 'what is a jQuery but a misunderstood object?';
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+var capitalize = function (sentence) {
+  var firstChar = sentence.charAt(0).toUpperCase();
+  var otherLetters = sentence.slice(1).toLowerCase();
+  var fullWord = firstChar + otherLetters;
+  return fullWord;
+}
+
 
 
 
@@ -72,3 +111,15 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+var vowelCounter = function(str) {
+  var total = 0;
+  var splitSen = str.split("");
+  console.log(splitSen);
+  for (var i = 0; i < splitSen.length; i++) {
+    if (splitSen[i] === "a" || splitSen[i] === "e" || splitSen[i] === "i" || splitSen[i] === "o" || splitSen[i] === "u") {
+      total += 1;   
+    }
+}
+return total
+}
